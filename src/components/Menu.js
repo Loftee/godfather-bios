94e5a555d073db4f5ccffc7293fb9bf543712e32
Andrew Collins <andrew@loftee.co.uk>
2019-07-09 20:30:33 +0100

    can render list of names as links

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,11 +5,24 @@ import './Menu.css';
 
 class Menu extends Component {
     render() {
+        const {employees} = this.props;
         return (
             <div className={'header'}>
                 <div className={'header__logo'}>
                     <img src={Logo} alt="The Godfather"/>
                 </div>
+                <ul className={'header__menu'}>
+                    {
+                        employees.map((employee, index) => {
+                                return (
+                                    <li key={index}>
+                                        <a className={'header__menu-link'} href={'#'+ employee.name}>{employee.name}</a>
+                                    </li>
+                                )
+                            }
+                        )
+                    }
+                </ul>
             </div>
         )
     }
--- src/components/Menu.test.js
+++ src/components/Menu.test.js
@@ -3,8 +3,23 @@ import {shallow} from "enzyme";
 import Menu from './Menu';
 
 describe('Menu Component', () => {
-   it('renders correctly', () => {
-      const component = shallow(<Menu />);
-      expect(component).toMatchSnapshot();
-   });
+    const props = {
+        employees: [
+            {
+                "name": "Vito Corleone",
+                "popularity": 5,
+                "biography": "Vito Andolini's story starts in Corleone, Sicily. In 1901, the local mafia chieftain, Don Ciccio, murders Vito's father Antonio when he refuses to pay him tribute. Paolo, Vito's older brother, swears revenge, but Ciccio's men kill him too. Ciccio then sends his men to fetch nine-year old Vito. However, Vito's mother insists on going as well and begs Ciccio to spare Vito. Ciccio refuses, reasoning the boy will seek revenge as a grown man. Upon Ciccio's refusal, Vito's mother holds a knife to Ciccio's throat, allowing her son to escape while Ciccio's men kill her. Family friends smuggle Vito out of Sicily, putting him on a ship with immigrants traveling to America. Ellis Island immigration officials rename him Vito Corleone, using his village for his surname. He later uses Andolini as his middle name in acknowledgement of his family heritage.",
+                "image": "Vito Corleone.jpg",
+                "colleagues": ["Carlo Rizzi", "Luci Mancini"]
+            }]
+    };
+    const component = shallow(<Menu {...props} />);
+
+    it('renders correctly', () => {
+        expect(component).toMatchSnapshot();
+    });
+
+    it('renders names as links', () => {
+        expect(component.find('.header__menu-link').length).toEqual(1);
+    });
 });
